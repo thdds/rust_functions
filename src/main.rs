f212028d0f447609614a64f10d7d2241c8f9a789
Thaddäus Schima <taddl@MacBookAir.fritz.box>
2025-08-21 20:55:28 +0200

    Waiting for the Ownership concepts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
+    another_function(5, 'T'); 
+    println!("{}", plus_one(58)); 
+}
+
+
+fn another_function(x: i32, unit_label: char) { 
+    println!("The value of x is {x}{unit_label}");
+} 
+
+
+// expressions vs statements 
+
+
+fn plus_one(x: i32) -> i32 { 
+    x + 1 
 }
